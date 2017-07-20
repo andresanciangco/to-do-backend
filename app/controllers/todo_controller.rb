aae3eb3c8b221ace736fb5eb0f5ee848a58fb347
Andre Sanciangco <jadsanciangco@gmail.com>
2017-07-20 18:03:59 +0000

    message

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,13 +2,13 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        todo_id="1"
+        todo_id = params[:id]
         if todo_id =="1"
             @todo_description="sleeping"
-            @todo_pomodoro_estimate="1"
-            elsif todo_id == "2"
+            @todo_pomodoro_estimate= 6
+        elsif todo_id == "2"
             @todo_description= "napping"
-            @todo_pomodoro_estimate
+            @todo_pomodoro_estimate = 6
         end
     end
 end
