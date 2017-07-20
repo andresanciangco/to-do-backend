3dd4ae0f66f71ef0e18a84b8875dc0ba8b03f416
Andre Sanciangco <jadsanciangco@gmail.com>
2017-07-20 19:05:49 +0000

    datadone

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,13 +2,6 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        todo_id = params[:id]
-        if todo_id =="1"
-            @todo_description="sleeping"
-            @todo_pomodoro_estimate= 6
-        elsif todo_id == "2"
-            @todo_description= "napping"
-            @todo_pomodoro_estimate = 6
-        end
+        @todo=Todo.find_by_id(params[:id])
     end
 end
