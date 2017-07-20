316511f0bd66f41390e8f3fd40b307450806ee8e
Andre Sanciangco <jadsanciangco@gmail.com>
2017-07-20 15:07:58 +0000

    message

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,13 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description="sleeping"
-        @todo_pomodoro_estimate=6
+        todo_id="1"
+        if todo_id =="1"
+            @todo_description="sleeping"
+            @todo_pomodoro_estimate="1"
+            elsif todo_id == "2"
+            @todo_description= "napping"
+            @todo_pomodoro_estimate
+        end
     end
 end
