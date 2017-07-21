f1f66c4b49ffe65c4be5cb0ba8cc18217776a422
Andre Sanciangco <jadsanciangco@gmail.com>
2017-07-21 14:17:25 +0000

    codenowdone

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,7 @@
 class TodoController < ApplicationController
    
     def index
+        @todos= Todo.all
     end
    
     def show
@@ -15,9 +16,22 @@ class TodoController < ApplicationController
         t = Todo.find_by_id(params[:id])
         t.description = params['description']
         t.pomodoro_estimate = params['pomodoro-estimate']
-        t.save redirect_to "/todo/show/#{t.id}" 
+        t.save 
+        redirect_to "/todo/show/#{t.id}" 
     end
-    
-    def delete
-    end
+   
+   def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro-estimate']
+        t.save 
+        redirect_to "/todo/show/#{t.id}" 
+   end 
+   
+   def destroy
+       t= Todo.find_by_id(params[:id])
+       t.destroy
+       t.save
+       redirect_to "/"
+   end
 end
